b8e0107ad7d17bd17183a7c08f65079fabe0c748
Akhil Dasari <137955305+Akhildasari5159@users.noreply.github.com>
2024-01-21 19:31:18 +0530

    files

---
+++ Object_Detection_Files/object.py
@@ -0,0 +1,67 @@
+import cv2
+
+classNames = []
+classFile = "/home/pi/Desktop/Object_Detection_Files/coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/pi/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/pi/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
+    objectInfo = []
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            className = classNames[classId - 1]
+            if len(objects) == 0 or className in objects:
+                objectInfo.append([box, className])
+                if draw:
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(
+                        img,
+                        classNames[classId - 1].upper(),
+                        (box[0] + 10, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1,
+                        (0, 255, 0),
+                        2,
+                    )
+                    cv2.putText(
+                        img,
+                        str(round(confidence * 100, 2)),
+                        (box[0] + 200, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        1,
+                        (0, 255, 0),
+                        2,
+                    )
+
+    return img, objectInfo
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+
+    while True:
+        success, img = cap.read()
+        
+        if not success:
+            print("Failed to capture frame. Exiting...")
+            break
+
+        result, objectInfo = getObjects(img, 0.45, 0.2)
+        cv2.imshow("Output", img)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
